d3ab9cb72ff112aa92f025fc9d2a15311bdc0a67
yarolit <yarillo@ngs.ru>
2023-06-16 18:35:01 +0600

    add main

--- predict.py
+++ predict.py
@@ -8,6 +8,10 @@ from catboost import Pool
 import preproc as prc
 
 
+with open('model.pkl', 'rb') as file_:
+    model = pickle.load(file_)
+
+
 def cat_num_split(df: pd.DataFrame) -> tuple:
     '''Ищем категориальные и числовые признаки в датафрейме'''
 
@@ -23,26 +27,34 @@ def cat_num_split(df: pd.DataFrame) -> tuple:
     return cat_columns, num_columns
 
 
-df = prc.load_data('./Datasets/test.csv')
+def make_pool(df: pd.DataFrame) -> Pool:
+    '''Преобразуем данные в специальный формат Catboost'''
 
-df = prc.get_predict_model_features(df)
+    return pred_data
 
-df = prc.create_adress_feature(df)
 
-pred = df.drop(['Столбец1', 'дом'], axis=1)
+def predict(df: pd.DataFrame) -> float:
+    '''Определяем цену c помощью модели'''
 
-X = pred
-features_names = list(pred.columns)
+    pred_data = Pool(data=df,
+                cat_features=cat_num_split(df)[0],
+                feature_names=list(df.columns))
 
-pred_data = Pool(data=X,
-                 cat_features=cat_num_split(pred)[0],
-                 feature_names=features_names)
+    return model.predict(pred_data)
 
-with open('model.pkl', 'rb') as file_:
-    model = pickle.load(file_)
 
-y_pred = model.predict(pred_data)
+if __name__ == "__main__":
 
-pred.insert(0, 'предполагаемая цена кв.м.', y_pred)
+    df = prc.load_data('./Datasets/test.csv')
 
-pred.to_csv('./Datasets/pred.csv', sep='\t', encoding='utf-16')
+    df = prc.get_predict_model_features(df)
+
+    df = prc.create_adress_feature(df)
+
+    pred = df.drop(['Столбец1', 'дом'], axis=1)
+
+    y_pred = predict(pred)
+
+    pred.insert(0, 'предполагаемая цена кв.м.', y_pred)
+
+    pred.to_csv('./Datasets/pred.csv', sep='\t', encoding='utf-16')
